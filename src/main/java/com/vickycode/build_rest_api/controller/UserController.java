510d186e3639f2706e21d94d0bc4ffb58fffc8ae
vigneshwaran <vigeshwarancse2002@gmail.com>
2025-10-28 15:12:23 +0530

    update my user controller

--- src/main/java/com/vickycode/build_rest_api/controller/UserController.java
+++ src/main/java/com/vickycode/build_rest_api/controller/UserController.java
@@ -16,5 +16,9 @@ public class UserController {
     public String DSAHello(){
         return "Prepare DSA Rounds";
     }
+    @GetMapping("backend")
+    public String BackendHello(){
+        return "Hello Backend";
+    }
 
 }
