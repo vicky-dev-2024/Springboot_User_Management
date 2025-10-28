94076b094dc87eaad47bea8010e6556ffa7f4b79
vicky-dev-2024 <vigeshwarancse2024@gmail.com>
2025-10-28 15:26:19 +0530

    changes updated

---
+++ src/test/java/com/vickycode/build_rest_api/BuildRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vickycode.build_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
